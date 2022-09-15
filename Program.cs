a5045895a9a4ca049a918567433ab49e0a5dd587
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-15 14:16:35 +0100

    Added missing fields

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace MeilisearchConsoleRunner
         {
             public string Id { get; set; }
             public string Title { get; set; }
+            public string Description { get; set; }
+            public string Actor { get; set; }
+            public int Age { get; set; }
             public string Genre { get; set; }
         }
 
@@ -21,12 +24,8 @@ namespace MeilisearchConsoleRunner
         {
             var client = new MeilisearchClient("http://localhost:7700", "masterKey");
             var index = await client.Index<Movie>("movies");
-            var movie = new Movie
-            {
-                Id = "123",
-                Title = "Pulp Fiction",
-                Genre = "Crime"
-            };
+            
+            
             await index.AddDocuments(new List<Movie> { movie });
             var searchResult = await index.Search<Movie>("Pulp Fiction");
             Console.WriteLine(searchResult);
